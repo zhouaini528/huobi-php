309ae3c243058614e5b27722dbc0887851a48633
lin <465382251@qq.com>
2021-04-09 10:58:53 +0800

    websocket reconnection time default 60s

--- src/Api/WebSocket/SocketServer.php
+++ src/Api/WebSocket/SocketServer.php
@@ -290,8 +290,8 @@ class SocketServer
                 echo PHP_EOL;
             }*/
 
-            //公共数据如果60秒内无数据更新，则断开连接重新订阅，重试次数不超过5次
-            if(isset($con->tag_data_time) && time()-$con->tag_data_time>60*($con->tag_reconnection_num+1) && $con->tag_reconnection_num<=5){
+            //公共数据如果60秒内无数据更新，则断开连接重新订阅，重试次数不超过10次
+            if(isset($con->tag_data_time) && time()-$con->tag_data_time>60*($con->tag_reconnection_num+1) && $con->tag_reconnection_num<=10){
                 if(in_array($con->tag,$this->public_url)) {
                     //public
                     $con->close();
